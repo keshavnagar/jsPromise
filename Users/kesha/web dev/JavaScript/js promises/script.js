6e6dcb22766ac82e32869ba05dc55c4a42c4d586
keshavnagar <keshavnagar887@gmail.com>
2025-08-27 22:33:59 +0530

    learnet the asynchronous behavior of js

---
+++ Users/kesha/web dev/JavaScript/js promises/script.js
@@ -0,0 +1,7 @@
+console.log("start")
+setTimeout(() => {
+    console.log("this thing is see after 5 second of execution of this program")
+}, 5000);
+console.log("end")
+
+// asychronous behaiviour of js
