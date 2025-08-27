19d5b10091b1ff3e17b91ec30ca56b01f67a7b0f
keshavnagar <keshavnagar887@gmail.com>
2025-08-27 23:00:29 +0530

    create a mini project on synchoursons and asynconous behavious of js

--- Users/kesha/web dev/JavaScript/js promises/script.js
+++ Users/kesha/web dev/JavaScript/js promises/script.js
@@ -1,6 +1,21 @@
-console.log("start")
-console.log("line 1")
-console.log("line 2")
-console.log("line 3")
-console.log("line 4")
-console.log("end")
+console.log("=== Program start ===");
+
+// syncronous function
+function greet(name) {
+  console.log("hello " + name);
+}
+
+// asyncronous function
+function delayedGreet(name, delay) {
+  setTimeout(() => {
+    console.log("hello " + name + " after " + delay / 1000 + " second");
+  }, delay);
+}
+
+//execution
+greet("keshav"); //syncronous
+delayedGreet("keshav", 4000); // asyncronous
+greet("ajay"); // syncronous
+delayedGreet("ajay", 2000); // asyncronous
+
+console.log("=== program end ===");
