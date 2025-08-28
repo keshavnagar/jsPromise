95d4adff524e4ad4437ba95e5aa8446b79520637
keshavnagar <keshavnagar887@gmail.com>
2025-08-28 22:17:20 +0530

    this is the initial learning of resolve function only

--- Users/kesha/jsPromise/script.js
+++ Users/kesha/jsPromise/script.js
@@ -1,10 +1,9 @@
 let p1 = new Promise((resolve, reject) => {
   let x = 90;
-  if(x==1)
+
  resolve(x); // calling by .then and x assign to .then()'s argument
-else
-reject(x);
+
 });
 
 p1.then((x) => console.log(`${x} : this is our resolve passed value`))
-.catch((x)=>console.log(`${x}`))
+
