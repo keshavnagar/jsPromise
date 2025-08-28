b6c0b9d7ad563fbc35b4446feeaad531bce1e6f0
keshavnagar <keshavnagar887@gmail.com>
2025-08-28 11:59:32 +0530

    this program for success == false

--- Users/kesha/web dev/JavaScript/js promises/script.js
+++ Users/kesha/web dev/JavaScript/js promises/script.js
@@ -1,7 +1,7 @@
 console.log("==== Programm start====");
 let asyncTask = new Promise((resolve, reject) => {
   setTimeout(() => {
-    let success = true;
+    let success = false;
     if (success) resolve("successufull " + "after " + 4 + " seconds");
     else reject("unsuccessful " + "after " + 4 + " second");
   }, 4000);
