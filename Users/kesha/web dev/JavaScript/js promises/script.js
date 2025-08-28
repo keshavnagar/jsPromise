531a8096d7e75954a7c551fa578d34cb47e898f1
keshavnagar <keshavnagar887@gmail.com>
2025-08-28 12:53:00 +0530

    this is simple exmaple of callback function

--- Users/kesha/web dev/JavaScript/js promises/script.js
+++ Users/kesha/web dev/JavaScript/js promises/script.js
@@ -1,17 +1,12 @@
-console.log("==== Programm start====");
-let asyncTask = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    let success = false;
-    if (success) resolve("successufull " + "after " + 4 + " seconds");
-    else reject("unsuccessful " + "after " + 4 + " second");
-  }, 4000);
-});
+// now we understood the call back function 
 
-asyncTask
-  .then((message) => {
-    console.log(message);
-  })
-  .catch((message) => {
-    console.log(message);
-  });
-console.log("==== Programm end====");
+function sum(x,y) {
+  return x+y;
+}
+
+function callBackFun (sum){
+return sum(1,2);
+}
+
+let  result = callBackFun(sum);
+console.log(result)
