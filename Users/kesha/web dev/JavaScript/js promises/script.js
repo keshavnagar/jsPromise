63ca13128ea2a2c4a74e5a517d6a753153d33ee4
keshavnagar <keshavnagar887@gmail.com>
2025-08-28 13:19:20 +0530

    mix settimeout in previosu too basic code that explain we recieve data in 2s

--- Users/kesha/web dev/JavaScript/js promises/script.js
+++ Users/kesha/web dev/JavaScript/js promises/script.js
@@ -1,5 +1,8 @@
-function getData(dataId){
-  console.log("data: "+dataId)
+function getData(dataId){//suppose it recieve the data after 2s, than we use setTimeOut
+  setTimeout(() => {
+     console.log("data: "+dataId)
+  }, 2000);
+ 
 }
 
 getData(789432)
