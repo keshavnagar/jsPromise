4d99e15e710d8c3ccfaa4f755a4e870d1b2e7fe4
keshavnagar <keshavnagar887@gmail.com>
2025-08-28 22:23:38 +0530

    we create so much .then () and it contains an above value that it holds

--- Users/kesha/jsPromise/script.js
+++ Users/kesha/jsPromise/script.js
@@ -1,9 +1,15 @@
 let p1 = new Promise((resolve, reject) => {
   let x = 90;
 
- resolve(x); // calling by .then and x assign to .then()'s argument
-
+  resolve(x); // calling by .then and x assign to .then()'s argument
 });
 
-p1.then((x) => console.log(`${x} : this is our resolve passed value`))
-
+p1.then((x) => {
+  console.log(`${x} : this is our resolve passed value`);
+  return x+5;
+}).then((x) => {
+  console.log(x);
+  return x+10;
+}).then((x)=>{
+  console.log(x)
+})
