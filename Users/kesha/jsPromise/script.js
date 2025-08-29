37e0b8a925a44c2276389ca32ad849c2be6cb191
keshavnagar <keshavnagar887@gmail.com>
2025-08-29 10:23:41 +0530

    learn the asyncrhounous task with the help of promise function

--- Users/kesha/jsPromise/script.js
+++ Users/kesha/jsPromise/script.js
@@ -1,18 +1,22 @@
+const ranNumGenDef = () => 
+  Math.floor(Math.random() * 10) + 1;
 let p1 = new Promise((resolve, reject) => {
-  let x = 90;
+  // let x = 90;
 
-  // resolve(x); // calling by .then and x assign to .then()'s argument
-  reject(x)
+  // // resolve(x); // calling by .then and x assign to .then()'s argument
+  // // reject(x)
+
+  const ranNumGen = ranNumGenDef();
+  setTimeout(resolve, 4000, ranNumGen);
 });
 
 p1.then((x) => {
-  console.log(`${x} : this is our resolve passed value`);
-  return x+5;
-}).then((x) => {
   console.log(x);
-  return x+10;
-}).then((x)=>{
-  console.log(x)
-}).catch((x)=>{
-  console.log(`${x} caught`)
+  return x * 2;
 })
+  .then((x) => {
+    console.log(x);
+  })
+  .catch((x) => {
+    console.log(`${x} caught`);
+  });
