6b5a554c12fac1d3ca2f9a40f16fd963ab55789d
keshavnagar <keshavnagar887@gmail.com>
2025-08-29 10:51:53 +0530

    learn another concept in which i learn the .then is also put when we use catch in this code like we used the then method only

--- Users/kesha/jsPromise/script2.js
+++ Users/kesha/jsPromise/script2.js
@@ -6,18 +6,22 @@ let p1 = new Promise((resolve, reject) => {
 
 let p2 = Promise.resolve(" p2 is resolved");
 let p3 = () => Promise.resolve(null);
-let p4 = () => Promise.reject(`p4 is rejected`)
+let p4 = () => Promise.reject(`p4 is rejected`);
 
-p1.then((result)=>{
-    console.log(`p1 : ${result}`);
-})
+p1.then((result) => {
+  console.log(`p1 : ${result}`);
+});
 
-p2.then((result)=>{
-    console.log(`p2 : ${result}`);
-})
+p2.then((result) => {
+  console.log(`p2 : ${result}`);
+});
 
-p3().then((result)=>{
-    console.log(`p3 : ${result}`);
-})
+p3().then((result) => {
+  console.log(`p3 : ${result}`);
+});
 
-p4().catch((failed) => console.log(`p4: ${failed}`))
+p4()
+  .then((result) => {
+    console.log(`${result}`);
+  })
+  .catch((result) => console.log(`p4: ${result}`));
