6aa2bc5516bc34dc78166a58c77698b0c1bcb4e5
keshavnagar <keshavnagar887@gmail.com>
2025-08-29 10:43:55 +0530

    learn the syntax of js promise call resolve different methods and calling things

---
+++ Users/kesha/jsPromise/script2.js
@@ -0,0 +1,21 @@
+let p1 = new Promise((resolve, reject) => {
+  let x = 100;
+  if (x == 100) resolve(" p1 is resolved");
+  else reject(" p1 is rejected");
+});
+
+let p2 = Promise.resolve(" p2 is resolved");
+let p3 = () => Promise.resolve(null);
+
+p1.then((result)=>{
+    console.log(`p1 : ${result}`);
+})
+
+p2.then((result)=>{
+    console.log(`p2 : ${result}`);
+})
+
+p3().then((result)=>{
+    console.log(`p3 : ${result}`);
+})
+
